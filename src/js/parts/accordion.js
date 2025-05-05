502ce8d4619edb75fbd7a1e142974c013bd0050c
Uzurch1k <galcenkovlad7@gmail.com>
2025-05-05 15:23:16 +0300

    add faq

--- src/js/parts/accordion.js
+++ src/js/parts/accordion.js
@@ -1,29 +1,18 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-// const accord = document.querySelectorAll('.accord');
+const accord = document.querySelectorAll('.accord');
 
-// accord?.forEach(list => {
-//   new Accordion(list, {
-//     duration: 400,
-//     showMultiple: false,
-//   });
+accord?.forEach(list => {
+  new Accordion(list, {
+    duration: 400,
+    showMultiple: false,
+  });
 
-//   const panels = list.querySelectorAll('.ac-panel');
-//   panels.forEach(panel => {
-//     panel.addEventListener('click', event => {
-//       event.stopPropagation();
-//     });
-//   });
-// });
-
-// <ul class="accord">
-//   <li class="ac">
-//     <div class="ac-trigger">
-//       <div class="__header"></div>
-//       <div class="ac-panel">
-//         <div class="__content"></div>
-//       </div>
-//     </div>
-//   </li>
-// </ul>
+  const panels = list.querySelectorAll('.ac-panel');
+  panels.forEach(panel => {
+    panel.addEventListener('click', event => {
+      event.stopPropagation();
+    });
+  });
+});
